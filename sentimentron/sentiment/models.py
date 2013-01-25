f6a483c7b738bdcc924b8771ac5ca6d2b2a2d232
Robert Myers <robert.myers@rackspace.com>
2013-01-25 14:12:11 -0600

    changing sentiment model

--- sentimentron/sentiment/models.py
+++ sentimentron/sentiment/models.py
@@ -5,18 +5,26 @@ from django.db import models
 
 # Generate a histogram of the stats for an entire year (non-leap).
 HISTOGRAM = """\
-SELECT AVG(sentiment_sentiment.score), 
-    DATE(sentiment_sentiment.timestamp)
+SELECT count(*),
+    score, 
+    DATE(timestamp)
     FROM sentiment_sentiment
-    GROUP BY DATE(sentiment_sentiment.timestamp)
-    LIMIT 365;
+    GROUP BY DATE(timestamp), score
+    LIMIT 1000;
 """
+
+
+SCORES = [
+    ('Promoter', 'promoter'),
+    ('Passive', 'passive'),
+    ('Detractor', 'detractor')
+]
 
 
 class Sentiment(models.Model):
     """An individual record of sentiment."""
     
-    score = models.IntegerField()
+    score = models.CharField(max_length=15, choices=SCORES)
     timestamp = models.DateTimeField()
     message = models.CharField(max_length=255)
     
@@ -29,5 +37,6 @@ class Sentiment(models.Model):
         from django.db import connection
         cursor = connection.cursor()
         cursor.execute(HISTOGRAM, [self.pk])
-        Day = namedtuple('Day', 'score timestamp')
-        return map(Day._make, cursor.fetchall())
+        Day = namedtuple('Day', 'count score date')
+        days = map(Day._make, cursor.fetchall())
+        return [dict(d._asdict()) for d in days]
